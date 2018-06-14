20fc7f2b266122e758b16b413e7a2db49d21e6ce
Catherine Umali <cumali@smg.local>
2018-06-14 16:57:00 +0800

    changes

--- resources/views/partials/lyric_page.blade.php
+++ resources/views/partials/lyric_page.blade.php
@@ -19,7 +19,7 @@
     <!-- <link href="font/font-awesome.min.css" rel="stylesheet" type="text/css"> -->
     <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
-</head>
+</head> 
 <body>
     <!-- Navigation -->
     <nav class="navbar navbar-default navbar-custom navbar-fixed-top">
